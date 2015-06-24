2e71f408d4e43318d2887d98872cec9bb8c70d44
pizzaboi <tophamhatt.crazy@gmail.com>
2015-06-24 19:21:01 +0900

    changed comments

--- tagger/output.py
+++ tagger/output.py
@@ -1,23 +1,29 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-src_cabocha = 'sentence.cabocha'
-src_tagged = 'sentence.tagged'
+src_cabocha = 'sentence.cabocha' # CaboCha解析結果
+src_tagged = 'sentence.tagged' # CRFSuiteの出力
 
+## CRFSuiteの付与結果を読み込み
 def get_labels():
-    L = []
+    L = [] # 意味ラベルリスト
     for line in open(src_tagged):
-        line = line.strip('\n')
+        line = line.strip("\n")
         L.append(line)
     return L
 
+## 出力
 def disp(L):
     for line in open(src_cabocha):
-        line = line.strip('\n')
-        if line.startswith(('#', '*', 'EOS')):
+        line = line.strip("\n")
+
+        ## 形態素行以外はそのまま出力
+        if line.startswith(("#", "*", "EOS")):
             print line
+
+        ## 意味ラベル + 形態素行
         else:
-            print L.pop(0) + '\t' + line
+            print L.pop(0) + "\t" + line
 
 if __name__ == '__main__':
     L = get_labels()
