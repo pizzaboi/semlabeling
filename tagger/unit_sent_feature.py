65ababc31e548fe0cf0e85795b95615d4ad0841f
pizzaboi <tophamhatt.crazy@gmail.com>
2015-06-24 19:32:44 +0900

    changed comments

--- tagger/unit_sent_feature.py
+++ tagger/unit_sent_feature.py
@@ -9,9 +9,9 @@ def read_sentence(names=('y','w','info'), sep='\t'):
     seq = []
     for line in sys.stdin:
         line = line.strip('\n')
-        if line.startswith(('#', '*')): #ignore non-morph line
+        if line.startswith(('#', '*')): # ignore non-morph line
             pass
-        elif line == 'EOS': # 文末でイテレート
+        elif line == 'EOS': # return at EOS
             return seq
         else:
             fields = ['NULL']
@@ -20,9 +20,9 @@ def read_sentence(names=('y','w','info'), sep='\t'):
                 raise ValueError("Invalid line: %s\n" % line)
             seq.append(dict(zip(names, tuple(fields[:3]))))
 
+## Apply templates, returning a feature in String.
+## @pram t: index of current morphme.
 def apply_template(seq, t, template):
-    """Apply templates and returns a feature in string
-    @param t: index of current morpheme."""
     name = '|'.join(['%s[%d]' % (f, o) for f, o in template]) #attr name
     values = []
     for field, offset in template: #get attrs
@@ -32,11 +32,12 @@ def apply_template(seq, t, template):
         values.append(seq[p][field])
     return '%s=%s' % (name, '|'.join(values))
 
+## escape colon
 def escape(src):
     return src.replace(':','__COLON__')
 
+## Extract features
 def extract_feature(EXTRACT_O=False):
-    """Extract features and write down features list"""
     templates =[]
 
     templates += [(('w',i),) for i in range(-2, 3)]
@@ -84,39 +85,35 @@ def extract_feature(EXTRACT_O=False):
         v['rd'] = morph_info[7] #read
         v['pr'] = morph_info[8] #pronunciation
 
-    #Extract features from not only FE sequence with B/I-label
-    #but others with O-label
+    ## Extract features regardless of Semantic labels.
     if EXTRACT_O:
         for t in range(len(seq)):
-            sys.stdout.write(seq[t]['y']) #ラベル出力
+            sys.stdout.write(seq[t]['y']) # output the label
             for template in templates:
                 attr = apply_template(seq, t, template)
                 if attr is not None:
-                    sys.stdout.write('\t%s' % escape(attr)) #素性出力
-            sys.stdout.write('\n') #ラベル区切り
-        sys.stdout.write('\n') #行区切り
+                    sys.stdout.write('\t%s' % escape(attr)) # output the features
+            sys.stdout.write('\n') # morph sep
+        sys.stdout.write('\n') # sentence sep
 
-    #Extract features from only FE sequence with B/I-label
+    ## Extract features from only FE sequence with B/I-label
     else:
-        for t in range(len(seq)): # 文中の全ての形態素に対して以下の処理
-            if seq[t]['y'] == 'O': #ignore O-label
+        for t in range(len(seq)):
+            if seq[t]['y'] == 'O': #ignore label O
                 continue
-            elif seq[t]['y'] == 'C': #述部区切り
+            elif seq[t]['y'] == 'C': # predicate sep
                 sys.stdout.write('\n')
                 #print
             elif seq[t]['y'].startswith(('B','I')): #Extract feature
-                sys.stdout.write(seq[t]['y']) #ラベル出力
+                sys.stdout.write(seq[t]['y']) # output the label
                 for template in templates:
                     attr = apply_template(seq, t, template)
                     if attr is not None:
-                        sys.stdout.write('\t%s' % escape(attr)) #素性出力
-                sys.stdout.write('\n') #ラベル区切り
+                        sys.stdout.write('\t%s' % escape(attr)) # output the features
+                sys.stdout.write('\n') # morph sep
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="",
-        epilog="",
-        fromfile_prefix_chars='@')
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
     parser.add_argument('-o','--extract-o', action='store_true',
         help='extract features from O-label.')
     args = parser.parse_args()
