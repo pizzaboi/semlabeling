3bab599e57b3ce77f189d3fa2c39b1f220ca59c6
pizzaboi <tophamhatt.crazy@gmail.com>
2015-06-24 19:25:44 +0900

    changed comment

--- tagger/run_tagger.sh
+++ tagger/run_tagger.sh
@@ -1,15 +1,22 @@
 #! /bin/sh
 
-#入力読み込み
+## USAGE:
+## echo "入力文" | bash run_tagger.sh
+
+## 入力文読み込み
 read sentence
 
-#
-# ここでエラー処理！
-#
+# (エラー処理が入る予定)
 
-#CaboCha > 素性抽出 > CRFSuite > 出力
+## CaboCha解析 > sentence.cabocha
 echo ${sentence} | /usr/local/bin/cabocha -f1 > sentence.cabocha
+
+## 素性抽出 > sentence.f
 python unit_sent_feature.py < sentence.cabocha > sentence.f
+
+## CRFSuiteによるタグ付与 > sentence.tagged
 #crfsuite tag -p -m 1627.m < sentence.f > sentence.tagged
 /usr/local/bin/crfsuite tag -m 1627.m < sentence.f > sentence.tagged
+
+## CaboCha解析結果と併せて出力
 python output.py
