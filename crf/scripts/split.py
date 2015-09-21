20f91bd80f630767db2d808963b44220e1d60552
pizzaboi <tophamhatt.crazy@gmail.com>
2015-09-21 18:42:57 +0900

    change directory

---
+++ crf/scripts/split.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+
+## Split files into N dataset equally, returning the list of splitted files.
+def split(directory, N=10):
+    seq = os.listdir(directory)
+    size = len(seq) / N
+    unit = [size] * N
+    remain = len(seq) - size * N
+    for i in range(remain):
+        unit[i] += 1
+    return [seq[sum(unit[:i]):sum(unit[:i])+unit[i]] for i in xrange(len(unit))]
+
+## Parse arguments.
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', help='target directory.')
+    parser.add_argument('dst', help='destination directory.')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    ## Write files in splitted N sets.
+    for i, seq in enumerate(split(args.dir)):
+        fo = open('%sdataset/%d' % (args.dst, i), 'w')
+        for f in seq:
+            fo.write(f+'\n')
