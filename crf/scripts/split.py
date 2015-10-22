e4b54e2b2dba2e4cfa68b2e80dde8b9726d37069
pizzaboi <tophamhatt.crazy@gmail.com>
2015-10-22 09:08:27 +0900

    change comment

--- crf/scripts/split.py
+++ crf/scripts/split.py
@@ -1,11 +1,10 @@
-#! /usr/bin/python
 # -*- coding: utf-8 -*-
 
 import argparse
 import os
 
-## Split files into N dataset equally, returning the list of splitted files.
 def split(directory, N=10):
+    """ディレクトリ内のファイルをN等分する関数．"""
     seq = sorted(os.listdir(directory))
     size = len(seq) / N
     unit = [size] * N
@@ -14,7 +13,6 @@ def split(directory, N=10):
         unit[i] += 1
     return [seq[sum(unit[:i]):sum(unit[:i])+unit[i]] for i in xrange(len(unit))]
 
-## Parse arguments.
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', help='target directory.')
@@ -25,7 +23,6 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
 
-    ## Write files in splitted N sets.
     for i, seq in enumerate(split(args.dir)):
         fo = open('%sdataset/%d' % (args.dst, i), 'w')
         for f in seq:
